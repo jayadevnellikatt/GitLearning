9086f8b323314f59d2a2893003685f7d15eeb514
Jayadev Nellikatt <nellikattjayadev@gmail.com>
2018-03-18 20:34:00 +0530

    added files for serilog folder

---
+++ Serilog/SerilogMVC/SerilogMVC/Controllers/HomeController.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using SerilogMVC.Models;
+
+using Microsoft.Extensions.Logging;
+
+using Serilog;
+using Serilog.Events;
+
+
+namespace SerilogMVC.Controllers
+{
+    public class Person
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; } 
+    }
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+        public IActionResult Index()
+        {
+            var p = new Person { FirstName = "Jay", LastName = "Nelli" };
+            Log.Logger = new LoggerConfiguration()
+            .Enrich.FromLogContext()
+            .WriteTo.RollingFile("log-{Date}.txt", LogEventLevel.Information)
+            .WriteTo.File("log.txt")
+            .WriteTo.Console()
+            .WriteTo.Seq("http://localhost:5341")
+            .CreateLogger();
+            Log.Logger.Information("Just trying the logger {@person}", p);
+            return View();
+        }
+        public IActionResult About()
+        {
+            ViewData["Message"] = "Your application description page.";
+
+            return View();
+        }
+
+        public IActionResult Contact()
+        {
+            ViewData["Message"] = "Your contact page.";
+
+            return View();
+        }
+
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ Serilog/SerilogMVC/SerilogMVC/Startup.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+using Microsoft.Extensions.Logging;
+using Serilog;
+using Serilog.Events;
+using Elasticsearch;
+using Serilog.Sinks.Elasticsearch;
+
+namespace SerilogMVC
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+        public Startup(IHostingEnvironment env)
+        {
+            //var configuration = new ConfigurationBuilder()
+            //.SetBasePath(env.ContentRootPath)
+            //.AddJsonFile("appsettings.json")
+            //.Build();
+
+            //Configure the Serilog pipeline
+            //Log.Logger = new LoggerConfiguration()
+            //    .MinimumLevel.Debug()
+            //    .Enrich.FromLogContext()
+            //    .WriteTo.RollingFile("log-{Date}.txt", LogEventLevel.Information)
+            //    .WriteTo.File("log.txt")
+            //    .WriteTo.Console()
+            //    .WriteTo.Seq("http://localhost:5341")
+            //    .WriteTo.Elasticsearch("localhost:9200")
+            //    .WriteTo.Elasticsearch(new ElasticsearchSinkOptions(new Uri("http://localhost:9200"))
+            //    {
+            //        AutoRegisterTemplate = true,
+            //    })
+            //    .ReadFrom.Configuration(configuration)
+
+            //    //var loggerConfig = new LoggerConfiguration()
+            //    //    .WriteTo.Elasticsearch(new ElasticsearchSinkOptions(new Uri("http://localhost:9200"))
+            //    //    {
+            //    //        AutoRegisterTemplate = true,
+            //    //    });
+            //    .CreateLogger();
+            //Log.Logger.Information("Executed by Dev at[Startup] {ExecutionTime}", Environment.TickCount);
+            //Log.Logger.Error("ElasticSearch:Kibana - This is a critical Error");
+            //Log.Logger.Warning("ElasticSearch:Kibana - Just a warning");
+        }
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddMvc();
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env, ILoggerFactory loggerFactory)
+        {
+            //loggerFactory.AddConsole(Configuration.GetSection("Logging"));
+            //loggerFactory.AddDebug();
+            //loggerFactory.AddSerilog();  //Add Serilog to the logging pipeline
+
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+                app.UseBrowserLink();
+            }
+            else
+            {
+                app.UseExceptionHandler("/Home/Error");
+            }
+            app.UseStaticFiles();
+            app.UseMvc(routes =>
+            {
+                routes.MapRoute(
+                    name: "default",
+                    template: "{controller=Home}/{action=Index}/{id?}");
+            });
+        }
+    }
+}
